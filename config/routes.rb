d19de240ece5bd37b1d30677ce66793ca1630f1e
Axel LoganMolbert <axldarealest@gmx.com>
2022-11-17 17:21:08 +0100

    cool phase 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: 'lists#index'
+
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+
+  # resources :bookmark, only: [:index, :new, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
